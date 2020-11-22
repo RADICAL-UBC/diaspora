85437be29bbe913a59e5221b492d548dc14a173d
Youssef <sharief@aucegypt.edu>
2020-11-21 17:42:05 -0800

    updated config

--- deployment/app.py
+++ deployment/app.py
@@ -1,6 +1,6 @@
-app_class = "diaspora.appexamples.minnietwitter.cloud.SimpleTwitterStart"
-app_name = "SimpleTwitter"
-app_client = "diaspora.appexamples.minnietwitter.device.generator.TwitterWorldGenerator"
+# app_class = "diaspora.appexamples.minnietwitter.cloud.SimpleTwitterStart"
+# app_name = "SimpleTwitter"
+# app_client = "diaspora.appexamples.minnietwitter.device.generator.TwitterWorldGenerator"
 
 app_class = "diaspora.appexamples.todolist.cloud.TodoStart"
 app_name = "ToDoList"
