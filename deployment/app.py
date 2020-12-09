4961844446f377a82105ea4b7f792048a7b4402b
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2020-12-09 10:21:36 -0800

    modified app.py

--- deployment/app.py
+++ deployment/app.py
@@ -1,6 +1,6 @@
-app_class = "diaspora.appexamples.minnietwitter.cloud.SimpleTwitterStart"
+app_class = "diaspora.appexamples.simpletwitter.cloud.SimpleTwitterStart"
 app_name = "SimpleTwitter"
-app_client = "diaspora.appexamples.minnietwitter.device.generator.TwitterWorldGenerator"
+app_client = "diaspora.appexamples.simpletwitter.device.generator.TwitterWorldGenerator"
 
 app_class = "diaspora.appexamples.todolist.cloud.TodoStart"
 app_name = "ToDoList"
